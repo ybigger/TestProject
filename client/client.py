e0a121d0904587b6e6fb54e72181adb379843731
ybigger <yanivbigger@gmail.com>
2017-08-21 22:28:26 +0300

    Restracture the server and enabling long messages at the client

--- client/client.py
+++ client/client.py
@@ -5,9 +5,17 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 9999 # TODO: read the port number from the UI file
 clientSocket.connect((host, port))
-clientSocket.send('test1.txt'.encode('ascii')) # TODO: read the file name from the UI
-serverMessage = clientSocket.recv(1024)
+clientSocket.send('dir1/test1.txt'.encode('ascii')) # TODO: read the file name from the UI
 
+serverMessage = ""
+while True:
+    data = clientSocket.recv(1024)    
+    if not data:
+        break
+    else:
+        serverMessage += data.decode('ascii')
+
+print("%s" % serverMessage)
 clientSocket.close()
 
-print("%s" % serverMessage.decode('ascii'))
+
--- server/server.py
+++ server/server.py
@@ -1,30 +1,59 @@
 import socket
 import json
 
-def handleFileRequest(path):
-    fileHandler = open(config_data['root_dir'] + path, 'r' )
-    return fileHandler.read()
+#Read the requested file or return an error in case the file is a directory
+#or if the path does  not exist
+def handleFileRequest(configData, path):
+    try:
+        fileHandler = open(configData['root_dir'] + path, 'r' )
+        return str(fileHandler.read())
+    except IOError:
+        errorMsg = "Failed to read " + path
+        print ("Failed to read " + configData['root_dir'] + path)
+        return errorMsg
 
-#Get the server configuration
-with open("config.json") as json_data:
-    config_data = json.load(json_data)
-    
-
-#Create the socket object
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = int(config_data['port'])
-serverSocket.bind((host, port))
-
-#Listen to incoming connection requests
-serverSocket.listen(5)
-
-while True:
-    #Establish a connection 
-    clientSocket, addr = serverSocket.accept()
-
+#Handle the client request
+def handleClientConnection(configData, clientSocket, addr):
     print ("Got a connection from %s" % str(addr))
     requestMessage = clientSocket.recv(1024)
-    responseMessage = handleFileRequest(str(requestMessage.decode('ascii')))
+    responseMessage = handleFileRequest(configData, requestMessage.decode('ascii'))
     clientSocket.send(responseMessage.encode('ascii'))
-    clientSocket.close()
+
+#Get the server configuration - config.json - the file is expected in the same
+#path as the server.py file and in a JSON format. The file should contain the 
+#port number to be used by the server and the root path for the server files.
+def getServerConfiguration():
+    with open("config.json") as jsonData:
+        configData = json.load(jsonData)
+    
+    return configData
+
+def startTheServerLoop(configData):
+    #Create the socket object
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = int(configData['port'])
+    serverSocket.bind((host, port))
+
+    #Listen to incoming connection requests
+    serverSocket.listen(5)
+
+    while True:
+        #Establish a connection 
+        clientSocket, addr = serverSocket.accept()
+
+        #Handle the connection
+        handleClientConnection(configData, clientSocket, str(addr))
+
+        #Close the connection
+        clientSocket.close()
+
+###############################################################################
+# Main
+###############################################################################
+
+#Read the configuration file
+configData = getServerConfiguration()
+
+#Create the server socket and handle requests
+startTheServerLoop(configData)
